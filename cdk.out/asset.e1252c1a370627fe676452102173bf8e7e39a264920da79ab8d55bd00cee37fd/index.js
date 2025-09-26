961bd03aaebc670f37dfe12f0a49b385be53fc9e
Hassan Isse <hassanisse@Hassans-MacBook-Pro.local>
2025-09-25 23:53:05 -0500

    Remove node_modules and cdk.out from repo and add .gitignore

--- cdk.out/asset.e1252c1a370627fe676452102173bf8e7e39a264920da79ab8d55bd00cee37fd/index.js
+++
@@ -1,8 +0,0 @@
-exports.handler = async (event) => {
-    console.log("Event:", JSON.stringify(event));
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Hello from ServerTask Lambda!" }),
-    };
-};
